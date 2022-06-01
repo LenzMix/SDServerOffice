adfbbe204e5a2ade1c91d5a22ec65b6c1103151f
lenzm <lenzm@LENZMIXPC>
2022-06-01 21:46:01 +0500

    Добавьте файлы проекта.

---
+++ ServerOffice/FileManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ServerOffice
+{
+    public static class FileManager
+    {
+        public static databaseConnector ReadFiles()
+        {
+            if (!System.IO.File.Exists(@"Database.xml"))
+            {
+                SaveFile(null);
+                return null;
+            }
+            string users = File.ReadAllText(@"Database.xml");
+
+            var xmlSerializer = new XmlSerializer(typeof(databaseConnector));
+            var stringReader = new StringReader(users);
+            databaseConnector userCollection = (databaseConnector)xmlSerializer.Deserialize(stringReader);
+            return userCollection;
+        }
+
+        private static void SaveFile(databaseConnector connector)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(databaseConnector));
+            StringWriter stringWriter = new StringWriter();
+            if (connector == null) 
+                xmlSerializer.Serialize(stringWriter, new databaseConnector
+                {
+                    databaseIP = "",
+                    databasePort = "",
+                    databaseUser = "",
+                    databasePassword = "",
+                    databaseName = "",
+                    databaseTable = "",
+                });
+            else 
+                xmlSerializer.Serialize(stringWriter, connector);
+            string userstring = stringWriter.ToString();
+            FileStream stream = null;
+            if (!File.Exists(@"Database.xml"))
+            {
+                //File.Delete(@"InfoUsers.xml");
+                stream = File.Create(@"Database.xml");
+                stream.Close();
+            }
+            File.WriteAllText(@"Database.xml", userstring);
+            if (stream != null) stream.Close();
+            stream = null;
+        }
+    }
+
+    [Serializable]
+    public class databaseConnector
+    {
+        [XmlElement("IP")]
+        public string databaseIP;
+        [XmlElement("Port")]
+        public string databasePort;
+        [XmlElement("User")]
+        public string databaseUser;
+        [XmlElement("Password")]
+        public string databasePassword;
+        [XmlElement("Name")]
+        public string databaseName;
+        [XmlElement("Table")]
+        public string databaseTable;
+    }
+}
---
+++ ServerOffice/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerOffice
+{
+    class Program
+    {
+        public static Thread KeyboardReader = null;
+        public static Thread Loader = null;
+        //public static EMode emode
+
+        public enum EMode
+        {
+            criticalCrash,
+            starting,
+            setup,
+            
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
